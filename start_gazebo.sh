e77c17593973785b6e3bd252a7c6603ee8414493
blckmn <blckmn@betaflight.com>
2026-02-20 21:51:14 +1100

    Realtime and headless (whilst testing)

--- start_gazebo.sh
+++ start_gazebo.sh
@@ -3,4 +3,4 @@
 export SDF_PATH=${PWD}/models:${SDF_PATH}
 export GZ_SIM_RESOURCE_PATH=${PWD}/worlds:${GZ_SIM_RESOURCE_PATH}
 export GZ_SIM_SYSTEM_PLUGIN_PATH=${PWD}/plugins/build:${GZ_SIM_SYSTEM_PLUGIN_PATH}
-gz sim -r -v 4 ${PWD}/worlds/$1
+gz sim -r -v 4 -s --headless-rendering ${PWD}/worlds/$1
